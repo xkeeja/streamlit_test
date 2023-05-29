e6a958a50f5f9a47c41e09f6208ccaa75e3fc639
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2023-05-29 16:52:27 +0900

    test

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ st.write('hello')
 
 with st.spinner('downloading...'):
     if st.button('click to download'):
-        url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
-        output = "scores.npy"
-        gdown.download(url, output, quiet=False)
-        st.write('successfully downloaded .npy')
+        # url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
+        # output = "scores.npy"
+        # gdown.download(url, output, quiet=False)
+        # st.write('successfully downloaded .npy')
 
-        similarity_matrix = load('scores.npy')
+        # similarity_matrix = load('scores.npy')
 
-        st.write(similarity_matrix.shape)
+        # st.write(similarity_matrix.shape)
 
 
         url_2 = "https://drive.google.com/u/0/uc?id=1Q_s55ingwAC4_vzAoJ3knXUqgExGpWzg&export=download"
@@ -33,13 +33,13 @@ with st.spinner('downloading...'):
         gdown.download(url_3, output_3, quiet=False)
         st.write('successfully downloaded anime.csv')
 
-        st.dataframe(pd.read_csv('anime_cleaned.csv'))
+        # st.dataframe(pd.read_csv('anime_cleaned.csv'))
 
 
-    def load_trainset():
-        reader = Reader(rating_scale=(0, 10))
-        scoredata = Dataset.load_from_df(ScoresDF_selected[['username', 'anime_id', 'my_score']], reader)
-        trainset = scoredata.build_full_trainset()
-        return trainset
+        def load_trainset():
+            reader = Reader(rating_scale=(0, 10))
+            scoredata = Dataset.load_from_df(ScoresDF_selected[['username', 'anime_id', 'my_score']], reader)
+            trainset = scoredata.build_full_trainset()
+            return trainset
 
-    trainset=load_trainset()
+        trainset=load_trainset()
