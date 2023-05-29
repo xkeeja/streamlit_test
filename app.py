7e0bf30fc300b1a00e0425f0822a94948b430cc8
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2023-05-29 11:43:19 +0900

    gdown

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import gdown
+from numpy import load
 
 st.write('hello')
+
+if st.button('click to download'):
+    url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
+    output = "scores.npy"
+    gdown.download(url, output, quiet=False)
+    st.write('successfully downloaded')
+
+similarity_matrix = load('scores.npy')
+
+st.write(similarity_matrix.shape)
