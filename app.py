52763cd26cefae9d2800c60b09bcd427421a8ffb
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2023-05-29 13:01:38 +0900

    gdown 2 & 3

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import gdown
 from numpy import load
+import pandas as pd
 
 st.write('hello')
 
@@ -9,8 +10,25 @@ with st.spinner('downloading...'):
         url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
         output = "scores.npy"
         gdown.download(url, output, quiet=False)
-        st.write('successfully downloaded')
+        st.write('successfully downloaded .npy')
 
         similarity_matrix = load('scores.npy')
 
         st.write(similarity_matrix.shape)
+
+
+        url_2 = "https://drive.google.com/u/0/uc?id=1Q_s55ingwAC4_vzAoJ3knXUqgExGpWzg&export=download"
+        output_2 = "ScoresDF_selected.csv"
+        gdown.download(url_2, output_2, quiet=False)
+        st.write('successfully downloaded scores.csv')
+
+        st.dataframe(pd.read_csv('ScoresDF_selected.csv'))
+
+
+
+        url_3 = "https://drive.google.com/u/0/uc?id=1cq_0KRmQzb2bnOr9yVG019DETOUimA88&export=download"
+        output_3 = "anime_cleaned.csv"
+        gdown.download(url_3, output_3, quiet=False)
+        st.write('successfully downloaded anime.csv')
+
+        st.dataframe(pd.read_csv('anime_cleaned.csv'))
