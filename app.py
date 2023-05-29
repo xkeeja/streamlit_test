03ab8695fb29118d03f39403226b49e863879914
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2023-05-29 17:22:58 +0900

    pickle

--- app.py
+++ app.py
@@ -2,29 +2,29 @@ import streamlit as st
 import gdown
 from numpy import load
 import pandas as pd
-from surprise import Dataset, Reader
+import pickle
 
 st.write('hello')
 
 with st.spinner('downloading...'):
     if st.button('click to download'):
-        # url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
-        # output = "scores.npy"
-        # gdown.download(url, output, quiet=False)
-        # st.write('successfully downloaded .npy')
+        url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
+        output = "scores.npy"
+        gdown.download(url, output, quiet=False)
+        st.write('successfully downloaded .npy')
 
-        # similarity_matrix = load('scores.npy')
+        similarity_matrix = load('scores.npy')
 
-        # st.write(similarity_matrix.shape)
+        st.write(similarity_matrix.shape)
 
 
-        url_2 = "https://drive.google.com/u/0/uc?id=1Q_s55ingwAC4_vzAoJ3knXUqgExGpWzg&export=download"
-        output_2 = "ScoresDF_selected.csv"
+        url_2 = "https://drive.google.com/u/0/uc?id=1fx_XfWa8e-sx5j5N5SCo4GHpMDBImFLo&export=download"
+        output_2= "trainset.obj"
         gdown.download(url_2, output_2, quiet=False)
-        st.write('successfully downloaded scores.csv')
+        st.write('successfully downloaded trainset.obj')
 
         # st.dataframe(pd.read_csv('ScoresDF_selected.csv'))
-        ScoresDF_selected = pd.read_csv('ScoresDF_selected.csv')
+        # ScoresDF_selected = pd.read_csv('ScoresDF_selected.csv')
 
 
 
@@ -36,10 +36,6 @@ with st.spinner('downloading...'):
         # st.dataframe(pd.read_csv('anime_cleaned.csv'))
 
 
-        def load_trainset():
-            reader = Reader(rating_scale=(0, 10))
-            scoredata = Dataset.load_from_df(ScoresDF_selected[['username', 'anime_id', 'my_score']], reader)
-            trainset = scoredata.build_full_trainset()
-            return trainset
-
-        trainset=load_trainset()
+        f=open('trainset.obj', 'rb')
+        trainset=pickle.load(f)
+        f.close()
