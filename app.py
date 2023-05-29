d2b0e40cd2861551adf3a4233123c5572fa72b1a
Roger Bian <113004083+xkeeja@users.noreply.github.com>
2023-05-29 11:47:06 +0900

    gdown

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from numpy import load
 
 st.write('hello')
 
-if st.button('click to download'):
-    url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
-    output = "scores.npy"
-    gdown.download(url, output, quiet=False)
-    st.write('successfully downloaded')
+with st.spinner('downloading...'):
+    if st.button('click to download'):
+        url = "https://drive.google.com/u/0/uc?id=1Ws4NjZH0d5OiEor3hRbNziC0KeWi1Vjh&export=download"
+        output = "scores.npy"
+        gdown.download(url, output, quiet=False)
+        st.write('successfully downloaded')
 
-similarity_matrix = load('scores.npy')
+        similarity_matrix = load('scores.npy')
 
-st.write(similarity_matrix.shape)
+        st.write(similarity_matrix.shape)
